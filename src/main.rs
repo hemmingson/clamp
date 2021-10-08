e187e5939ce449b13f5ac07cdc7c9b64ba317d55
hemmingson <287969392@qq.com>
2021-10-08 23:47:43 +0800

    feat: add rand function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
+use rand::Rng;
 // [🧑‍🏫] import { io } from 'std'
 use std::io;
 
 // !🦀
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=25);
     // [🧑‍🏫] const crab = 0
     let _crab = 0; // immutable
     // [🧑‍🏫] let crab = string.new()
     let mut crab = String::new(); // mutable
 
-    println!("what's name of this crab?");
+    println!("what's age of this crab?");
 
     // '&'means refrence
     io::stdin().read_line(&mut crab).expect("utf-8");
 
     // [🧑‍🏫] console.log('%s', crab)
-    println!("crab is {}.", crab);
+    println!("crab is {}. >{}", secret_number, crab);
 }
